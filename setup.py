13a17c56a3f40a37228a4190c7cdab5fb0c9dc01
jssuzanne <jssuzanne@anybox.fr>
2016-06-28 01:52:29 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -15,13 +15,17 @@ requires = [
 ]
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst')) as readme:
+with open(os.path.join(here, 'README.rst'), 'r', encoding='utf-8') as readme:
     README = readme.read()
 
-with open(os.path.join(here, 'doc', 'FRONT.rst')) as front:
+with open(
+    os.path.join(here, 'doc', 'FRONT.rst'), 'r', encoding='utf-8'
+) as front:
     FRONT = front.read()
 
-with open(os.path.join(here, 'doc', 'CHANGES.rst')) as change:
+with open(
+    os.path.join(here, 'doc', 'CHANGES.rst'), 'r', encoding='utf-8'
+) as change:
     CHANGE = change.read()
 
 
@@ -45,7 +49,7 @@ setup(
     description="Beaker session for AnyBlok / Pyramid",
     license="MPL2",
     long_description=README + '\n' + FRONT + '\n' + CHANGE,
-    url="",
+    url="http://docs.anyblok-pyramid-beaker.anyblok.org/" + version,
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
